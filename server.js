4c7769faed817e225905ee893d8911ed5c74d718
Parampreet Singh <114291795+ParampreetSingh23@users.noreply.github.com>
2025-02-20 10:36:05 +0530

    19Feb

--- server.js
+++ server.js
@@ -1,71 +1,62 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 
-//app.get("/profile",(req,res)=>{
-//    const {username} =req.query;
-//    res.send("profile page of "+" " + username)
-//})
-
-app.get("/profile",(req,res)=>{
-    const {username, age , address} =req.query;
-    res.send("profile page of "+" " + username + " " + age + " " + address)
-})
-
-let userData=[
-   {
-     id:1,
-     name :"amit",
-     address : "fdk"
-   },
-   {
-        id:2,
-        name : "arti",
-        address : "bti"
-   },
-   {
-        id:3,
-        name : "abhinav",
-        address :"fzr"
-   }
-]
-//bhejna - get
-app.get("/allusers",(req,res)=>{
-    res.send(userData);
-})
-
-app.get("/getuserbyId",(req,res)=>{
-    let {id} = req.query;
-    for(let i = 0 ; i<userData.length ; i++){
-         if(userData[i].id == id){
-            return res.send(userData[i])
-         }
-    }
-    res.send("user not found")
-})
-
-app.get("/deleteuserId",(req,res)=>{
- let {id} = req.query;
- for(let i = 0;i<userData.length;i++){
-  if(userData[i].id==id){
-   userData.splice(i,1);
-   return res.send("user deleted")
-  }
-  res.send("No user found")
- }
-
-})
-app.get("/add",(req,res)=>{
-
-const { id, name, address } = req.query;
-
-
-userData.push({
-    id,
-    name,
-    address
+// Profile Route with Parameters
+app.get("/profile/:username/:age/:address", (req, res) => {
+    const { username, age, address } = req.params;
+    res.send(`Profile page of ${username} ${age} ${address}`);
 });
-res.send("Added")
-})
-app.listen(3002,()=>{
-    console.log("Server started");
-})
+
+// Sample user data
+let userData = [
+    { id: 1, name: "amit", address: "fdk" },
+    { id: 2, name: "arti", address: "bti" },
+    { id: 3, name: "abhinav", address: "fzr" }
+];
+
+// Get all users
+app.get("/allusers", (req, res) => {
+    res.send(userData);
+});
+
+// Get user by ID
+app.get("/getuserbyId/:id", (req, res) => {
+    let { id } = req.params;
+    //id = parseInt(id); // Convert ID to integer
+
+    for (let i = 0; i < userData.length; i++) {
+        if (userData[i].id === id) {
+            return res.send(userData[i]);
+        }
+    }
+    res.send("User not found");
+});
+
+// Delete user by ID
+app.get("/deleteuserById/:id", (req, res) => {
+    let { id } = req.params;
+    //id = parseInt(id);
+
+    for (let i = 0; i < userData.length; i++) {
+        if (userData[i].id === id) {
+            userData.splice(i, 1); // Remove user from array
+            return res.send("User deleted");
+        }
+    }
+    res.send("No user found to delete");
+});
+
+// Add new user
+app.get("/addUser/:id/:name/:address", (req, res) => {
+    let { id, name, address } = req.params;
+    //id = parseInt(id);
+
+    let newUser = { id, name, address };
+    userData.push(newUser);
+    res.send("New user added successfully");
+});
+
+// Start server
+app.listen(3002, () => {
+    console.log("Server started on port 3002");
+});
